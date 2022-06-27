9cdd753ff676975e3c200e58a409e025dc539b5d
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2022-06-27 20:08:30 +0200

    Add files via upload

---
+++ entete.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Timetracking</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="styles.css" type="text/css">
+    </head>
+    <body>
+        <br><br><br>
+        <div id="navbar" class="navbar navbar-expand-sm justify-content-center bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a id="menu" class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a id="menu" class="nav-link" href="creation.php">Créer une nouvelle tâche</a>
+                </li>
+            </ul>
+        </div>
+        <br><br>
